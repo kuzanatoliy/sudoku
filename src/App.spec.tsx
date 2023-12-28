88dbf7f56a656b6d915c426a29a7bfc5efb50017
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-12-28 22:02:38 +0300

    test: S-22 cover App component

---
+++ src/App.spec.tsx
@@ -0,0 +1,30 @@
+import { render } from '@solidjs/testing-library';
+import { beforeEach, describe, expect, it, vitest } from 'vitest';
+
+import { SudokuPlay } from 'components';
+
+vitest.mock('components', async () => {
+  const origin =
+    await vitest.importActual<typeof import('components')>('components');
+  return {
+    ...origin,
+    SudokuPlay: vitest
+      .fn()
+      .mockImplementation((props) => <div>{JSON.stringify(props)}</div>),
+  };
+});
+
+import App from './App';
+
+describe('App', () => {
+  const renderComponent = () => render(() => <App />);
+
+  beforeEach(() => {
+    vitest.clearAllMocks();
+  });
+
+  it('Should render component', () => {
+    renderComponent();
+    expect(SudokuPlay).toBeCalled();
+  });
+});
