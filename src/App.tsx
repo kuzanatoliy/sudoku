de5008d41098bb196ab7935bab74e47f8b094cd4
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2023-12-28 21:48:22 +0300

    feat: S-22 implement homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,15 @@
-import { createSignal } from 'solid-js';
-import solidLogo from './assets/solid.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { SudokuPlay } from 'components';
+import { TSudokuValue } from 'sudoku-engine';
 
-function App() {
-  const [count, setCount] = createSignal(0);
-  return (
-    <>
-      <div>
-        <a href='https://vitejs.dev' target='_blank'>
-          <img src={viteLogo} class='logo' alt='Vite logo' />
-        </a>
-        <a href='https://solidjs.com' target='_blank'>
-          <img src={solidLogo} class='logo solid' alt='Solid logo' />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class='card'>
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class='read-the-docs'>
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
-  );
-}
+import data from '../data/plays.json';
+import styles from './App.module.scss';
+
+import '../src/theme.scss';
+
+const App = () => (
+  <div class={styles.app}>
+    <SudokuPlay initialPlay={data[0].play as TSudokuValue[]} />
+  </div>
+);
 
 export default App;
