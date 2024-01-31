8807343fa9ef8293ec11efd9b1b259f2eeff942d
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-01-31 14:54:23 +0300

    test: S-32 check tests with isolation

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ const vitestConfig = defineTestConfig({
     pool: 'threads',
     poolOptions: {
       threads: {
-        isolate: false,
+        isolate: true,
       },
     },
     include: ['src/**/*{test,spec}.{js,ts,jsx,tsx}'],
