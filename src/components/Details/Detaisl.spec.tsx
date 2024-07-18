4cf301c167c3fdc2221146346f1a5b88c71fda61
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-07-18 23:13:31 +0300

    test: S-46 coverage

---
+++ src/components/Details/Detaisl.spec.tsx
@@ -0,0 +1,33 @@
+import { render, screen } from '@solidjs/testing-library';
+import { beforeEach, describe, expect, it, vitest } from 'vitest';
+
+import { Details, IDetailsProps } from './Details';
+
+describe('Details', () => {
+  const DEFAULT_SUMMARY = 'DETAILS SUMMARY';
+  const DEFAULT_CONTENT = 'DETAILS CONTENT';
+
+  const renderComponent = (props: Partial<IDetailsProps> = {}) => {
+    return render(() => (
+      <Details
+        summary={DEFAULT_SUMMARY}
+        children={DEFAULT_CONTENT}
+        {...props}
+      />
+    ));
+  };
+
+  beforeEach(() => {
+    vitest.clearAllMocks();
+  });
+
+  it('Should hide content by default', () => {
+    renderComponent({ isAutoOpened: false });
+    expect(screen.getByText(DEFAULT_CONTENT)).not.toBeVisible();
+  });
+
+  it('Should display content by default', () => {
+    renderComponent({ isAutoOpened: true });
+    expect(screen.getByText(DEFAULT_CONTENT)).toBeVisible();
+  });
+});
