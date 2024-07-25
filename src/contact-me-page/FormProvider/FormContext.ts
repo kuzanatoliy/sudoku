e989622f08164f153878908a894a301e88749936
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-07-25 22:46:47 +0300

    feat: S-43 workflow of contact me page

--- src/contact-me-page/FormProvider/FormContext.ts
+++
@@ -1,7 +0,0 @@
-import { createContext, Accessor } from 'solid-js';
-
-import { TFormState } from './types';
-
-export const FormContext = createContext<Accessor<TFormState>>(() => {
-  throw 'Error: It is neccessary to use FormProvider';
-});
--- src/contact-me-page/FormProvider/types/form-state.types.ts
+++
@@ -1,3 +0,0 @@
-export type TFormState = {
-  firstName: string;
-};
