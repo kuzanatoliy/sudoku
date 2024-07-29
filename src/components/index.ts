3da28c4c41bce1137813d687b51fd925b49766a9
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-07-30 00:32:36 +0300

    feat: S-50 update imports

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,7 @@
 export * from './Button';
 export * from './Details';
 export * from './DeviceProvider';
+export * from './Note';
+export * from './Pill';
 export * from './TextareaField';
 export * from './TextField';
