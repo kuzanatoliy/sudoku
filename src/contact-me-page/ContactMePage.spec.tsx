9469be5b1f9defc4e2d989b50c49b3de8b9d95bf
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-09-03 23:09:00 +0300

    test: S-43 cover ContactMePage

---
+++ src/contact-me-page/ContactMePage.spec.tsx
@@ -0,0 +1,80 @@
+import { render, screen } from '@solidjs/testing-library';
+import userEvent from '@testing-library/user-event';
+import { beforeEach, describe, expect, it, vitest } from 'vitest';
+import { useQuery } from 'query';
+
+import { ContactMePage } from './ContactMePage';
+
+vitest.mock('query', async () => {
+  const origin = await vitest.importActual<typeof import('query')>('query');
+  return {
+    ...origin,
+    useQuery: vitest.fn(),
+  };
+});
+
+describe('ContactMePage', () => {
+  const runQuerySpy = vitest.fn();
+
+  const renderComponent = () => {
+    vitest.mocked(useQuery).mockImplementation(() => ({
+      state: vitest.fn().mockReturnValue({
+        isLoading: false,
+        isStarted: false,
+        isValid: true,
+        isError: false,
+        data: undefined,
+      }),
+      runQuery: runQuerySpy,
+    }));
+    return render(() => <ContactMePage />);
+  };
+
+  beforeEach(() => {
+    vitest.clearAllMocks();
+  });
+
+  it('Should render component', () => {
+    renderComponent();
+    expect(screen.getByRole('button', { name: 'Submit' })).toBeDefined();
+    expect(screen.getByRole('button', { name: 'Reset' })).toBeDefined();
+    expect(screen.getByRole('textbox', { name: 'First name:' })).toBeDefined();
+    expect(screen.getByRole('textbox', { name: 'Last name:' })).toBeDefined();
+    expect(screen.getByRole('textbox', { name: 'Email:' })).toBeDefined();
+    expect(screen.getByRole('textbox', { name: 'Subject:' })).toBeDefined();
+    expect(screen.getByRole('textbox', { name: 'Message:' })).toBeDefined();
+  });
+
+  it('Should validate workflow', async () => {
+    renderComponent();
+    await userEvent.type(
+      screen.getByRole('textbox', { name: 'First name:' }),
+      'firstname'
+    );
+    await userEvent.type(
+      screen.getByRole('textbox', { name: 'Last name:' }),
+      'lastname'
+    );
+    await userEvent.type(
+      screen.getByRole('textbox', { name: 'Email:' }),
+      'email@hm.com'
+    );
+    await userEvent.type(
+      screen.getByRole('textbox', { name: 'Subject:' }),
+      'subject'
+    );
+    await userEvent.type(
+      screen.getByRole('textbox', { name: 'Message:' }),
+      'message'
+    );
+    await userEvent.click(screen.getByRole('button', { name: 'Submit' }));
+    expect(runQuerySpy).toHaveBeenCalledWith({
+      body: {
+        subject: 'subject',
+        name: `firstname lastname`,
+        email: 'email@hm.com',
+        message: 'message',
+      },
+    });
+  });
+});
--- src/contact-me-page/ContactMePage.tsx
+++ src/contact-me-page/ContactMePage.tsx
@@ -178,7 +178,7 @@ export const ContactMePage: TParentComponent = () => {
             });
           }}
         >
-          Submin
+          Submit
         </Button>
       </div>
     </div>
