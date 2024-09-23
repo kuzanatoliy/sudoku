bf1de5ae7440263788c5a4c7b5f8fa9a75d37b8b
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-09-23 22:05:29 +0300

    feat: S-36 redesign

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,25 @@
-import { HashRouter as Router, Route } from '@solidjs/router';
+import { HashRouter as Router, Route, Navigate } from '@solidjs/router';
 
 import { TParentComponent } from 'types';
 import { DeviceProvider } from 'components';
-
-import { Layout } from './Layout';
+import { HomePage } from 'home-page';
+import { SudokuPage } from 'sudoku-page';
+import { ContactMePage } from 'contact-me-page';
+import { NotFoundPage } from 'not-found-page';
 
 import '../src/theme.scss';
-import { HomePage } from 'home-page';
+import { Layout } from './Layout';
 
 export const App: TParentComponent = () => (
   <DeviceProvider>
     <Router>
       <Route path='/home' component={HomePage} />
-      <Route path='*' component={Layout} />
+      <Route path='/*' component={Layout}>
+        <Route path='/contact-me' component={ContactMePage} />
+        <Route path='/play' component={SudokuPage} />
+        <Route path='/' component={() => <Navigate href='/home' />} />
+        <Route path='*' component={NotFoundPage} />
+      </Route>
     </Router>
   </DeviceProvider>
 );
--- src/home-page/HomePage.tsx
+++ src/home-page/HomePage.tsx
@@ -1,16 +1,23 @@
+import { useNavigate } from '@solidjs/router';
 import { TParentComponent } from 'types';
 
 import styles from './HomePage.module.scss';
 import { Button, EButtonVariant } from 'components';
 
 export const HomePage: TParentComponent = () => {
+  const navigate = useNavigate();
   return (
-    <>
-      <main class={styles.container}>
+    <div class={styles.container}>
+      <main class={styles.main}>
         <h1 class={styles.header}>Sudoku</h1>
-        <Button variant={EButtonVariant.OUTLINED}>Go to play</Button>
+        <Button
+          onClick={() => navigate('/play')}
+          variant={EButtonVariant.OUTLINED}
+        >
+          Go to play
+        </Button>
       </main>
       <footer class={styles.app_footer}>© Kuzanatoliorg</footer>
-    </>
+    </div>
   );
 };
