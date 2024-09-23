384bcc0a86aad0e121ebb0546e449f96af466b84
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2024-09-23 22:36:48 +0300

    test: S-36 cover HomePage

---
+++ src/home-page/HomePage.spec.tsx
@@ -0,0 +1,40 @@
+import { render, screen, fireEvent } from '@solidjs/testing-library';
+import { beforeEach, describe, expect, it, vitest } from 'vitest';
+
+import { HomePage } from './HomePage';
+import { useNavigate } from '@solidjs/router';
+
+const navigateSpy = vitest.fn();
+vitest.mock('@solidjs/router', async () => {
+  const origin =
+    await vitest.importActual<typeof import('@solidjs/router')>(
+      '@solidjs/router'
+    );
+  return {
+    ...origin,
+    useNavigate: vitest.fn().mockImplementation(() => navigateSpy),
+  };
+});
+
+describe('HomePage', () => {
+  const renderComponent = () => {
+    return render(() => <HomePage />);
+  };
+
+  beforeEach(() => {
+    vitest.clearAllMocks();
+  });
+
+  it('Should render component', () => {
+    renderComponent();
+    expect(screen.getByRole('heading', { name: 'Sudoku' })).toBeDefined();
+    expect(screen.getByRole('button', { name: 'Go to play' })).toBeDefined();
+    expect(useNavigate).toBeCalled();
+  });
+
+  it('Should render SudokuPage', () => {
+    renderComponent();
+    fireEvent.click(screen.getByRole('button'));
+    expect(navigateSpy).toBeCalled();
+  });
+});
