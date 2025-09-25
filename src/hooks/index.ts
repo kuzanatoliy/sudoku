3bd328120245e45475bde80f7c3cd09285373337
Anatoli Kuzmiankou <anatoli_kuzmiankou@epam.com>
2025-09-25 16:20:18 +0300

    feat: S-81 remove use debounced value hook / not works as expected

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -1,5 +1,4 @@
 export * from './use-debounced-function';
-export * from './use-debounced-value';
 export * from './use-disclosure';
 export * from './use-leaving-confirmation';
 export * from './use-one-level-object';
--- src/hooks/use-debounced-value/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './use-debounced-value';
